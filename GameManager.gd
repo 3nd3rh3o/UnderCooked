f73a2c3182525c1ac4ecb892591c63907f077fd9
HugoGELLIDA <hugo.gellida34@gmail.com>
2025-09-19 16:57:45 +0200

    GameManager start

--- GameManager.gd
+++ GameManager.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
-func dupe(objectName, coordinates):
+func dupe(objectName: String, coordinates: Vector3):
 	var object = self.find_child(objectName)
-	var copy = object.duplicate()
-	
+	var copy: Node3D = object.duplicate()
+	self.add_child(copy)
+	copy.position = coordinates;
+	copy.visible = true
+
+
 func _ready():
-	dupe("BakingPan", 0)
+	dupe("tomatoGenerator", Vector3(6, 0, 2))
