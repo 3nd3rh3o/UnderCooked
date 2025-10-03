e06f49c8818cd0670f1b139225d9cffbd2c5214f
myaalicewendy <99797519+myaalicewendy@users.noreply.github.com>
2025-10-03 17:10:05 +0200

    Update stand.gd

--- scripts/stand.gd
+++ scripts/stand.gd
@@ -2,7 +2,8 @@ extends Resource
 
 class_name Stand
 
-@export var aviable_recipes: Array[Recipe]
-@export var processing_recipes: Array[Recipe]
+@export var available_recipes: Array[Recipe]
 @export var processed_recipe: Recipe
 @export var processing_time: float
+@export var accept_movable_object : bool
+@export var movable_object : Array[movable_stand]
